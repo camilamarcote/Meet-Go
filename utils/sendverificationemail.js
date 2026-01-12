f09772f66b35d587b8c106005818339270ea38e2
camilamarcote <marcotecamila@gmail.com>
2026-01-12 13:00:42 -0300

    agregando verify.js

--- utils/sendverificationemail.js
+++ utils/sendverificationemail.js
@@ -3,7 +3,18 @@ import { Resend } from "resend";
 const resend = new Resend(process.env.RESEND_API_KEY);
 
 export const sendVerificationEmail = async (email, token) => {
-  const link = `${process.env.FRONTEND_URL}/verify.html?token=${token}`;
+  const FRONT_URL =
+    process.env.FRONTEND_URL ||
+    process.env.FRONT_URL || // 👈 ESTA ES LA CLAVE EN TU CASO
+    "https://meetandgof.netlify.app";
+
+  if (!FRONT_URL) {
+    throw new Error("FRONTEND_URL / FRONT_URL no definida");
+  }
+
+  const link = `${FRONT_URL}/verify.html?token=${token}`;
+
+  console.log("🔗 Verification link:", link); // 🔍 se ve en Render logs
 
   await resend.emails.send({
     from: "Meet&Go <onboarding@resend.dev>",
@@ -15,7 +26,4 @@ export const sendVerificationEmail = async (email, token) => {
       <a href="${link}">Confirmar mi cuenta</a>
     `
   });
-
-  console.log("🔗 Verification link:", link);
-
 };
